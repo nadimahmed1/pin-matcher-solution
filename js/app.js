feb966452cc8efd3fec3fc84b14567430bf96d27
nadimahmed1 <nadimarafat26@gmail.com>
2022-06-07 08:37:06 +0600

    last commit

--- js/app.js
+++ js/app.js
@@ -9,8 +9,38 @@ function getPin() {
     }
 }
 
-
+// 
 function generatePin() {
     const generateInput = document.getElementById('display-pin');
     generateInput.value = getPin();
+}
+// 
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('calc-input');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
+//
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typed = document.getElementById('calc-input').value;
+    const success = document.getElementById('success');
+    const error = document.getElementById('error');
+    if (typed == pin) {
+        success.style.display = 'block';
+        error.style.display = 'none';
+    }
+    else {
+        error.style.display = 'block';
+        success.style.display = 'none';
+    }
 }
